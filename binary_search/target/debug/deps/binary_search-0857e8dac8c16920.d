67a6966f9f1f57616d316d5414da2d1647a5ceac
Zuev <165550286+dvrcky@users.noreply.github.com>
2025-01-03 18:52:06 +0400

    Delete binary_search/target directory

--- binary_search/target/debug/deps/binary_search-0857e8dac8c16920.d
+++
@@ -1,5 +0,0 @@
-/home/dvrcky/projects/rust/alghoritms/binary_search/target/debug/deps/libbinary_search-0857e8dac8c16920.rmeta: src/main.rs
-
-/home/dvrcky/projects/rust/alghoritms/binary_search/target/debug/deps/binary_search-0857e8dac8c16920.d: src/main.rs
-
-src/main.rs:
